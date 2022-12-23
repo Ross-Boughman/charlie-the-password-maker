d5816097f1808440ed49020b248f31206e60bef4
JSlander <jhovanslander@hotmail.com>
2022-12-22 22:23:34 -0600

    fixed one extra character and added comments

--- assets/script.js
+++ assets/script.js
@@ -20,28 +20,36 @@ generateBtn.addEventListener("click", writePassword);
 
 // creates random password and posts it for user to view
 function generatePassword() {
+  // adds user selections to characters
   var characters = "";
   var passwordLength = prompt("How long would you like your password? Between 8-128");
   var upperAsk = confirm("Include uppercase characters?");
   var lowerAsk = confirm("Include lowercase characters?");
   var numberAsk = confirm("Include numbers?");
   var specialAsk = confirm("Include special characters?");
+  // creates blank password variable
   var password = "";
+  // assigns uppercase letters to characters variable
   if (upperAsk === true) { 
     characters += upper; 
   } 
+  // assigns lowercase letters to characters variable
   if (lowerAsk === true) { 
     characters += lower; 
   } 
+  // assigns numbers to characters variable
   if (numberAsk === true) { 
     characters += number; 
   } 
+  // assigns special characters to characters variable
   if (specialAsk === true) { 
     characters += special; 
   } 
-  for (let index = 0; index <= passwordLength; index++) {
+  // generates random password based on modified characters variable
+  for (let index = 1; index <= passwordLength; index++) {
   var randomNumber = Math.floor(Math.random() * characters.length);
   password += characters.substring(randomNumber, randomNumber +1);
   }
+  // returns generated password to be posted to text box
   return password
 }
